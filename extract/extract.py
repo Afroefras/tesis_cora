242d859455d8e8a5f3c8a8e54d6b5ff7ab99cb2d
EF <efraisma.ef7@gmail.com>
2023-12-26 18:09:12 -0600

    Update extract.py
    Read .dat instead of .wav

--- extract/extract.py
+++ extract/extract.py
@@ -4,19 +4,19 @@ from pathlib import Path, PosixPath
 
 def get_all_heartpairs(base_dir: str) -> List[Tuple[PosixPath, PosixPath]]:
     ghp = GetHeartPairs(base_dir)
-    return [ghp.pair_wav_dat(x) for x in ghp.get_all_wav()]
+    return [ghp.pair_dat_wav(x) for x in ghp.get_all_dat()]
 
 
 class GetHeartPairs:
     def __init__(self, base_dir: str) -> None:
         self.base_dir = Path(base_dir)
 
-    def get_all_wav(self) -> Generator[PosixPath, None, None]:
-        wav_dirs = self.base_dir.glob("*/*.wav")
-        return wav_dirs
+    def get_all_dat(self) -> Generator[PosixPath, None, None]:
+        dat_dirs = self.base_dir.glob("*/*.dat")
+        return dat_dirs
 
-    def pair_wav_dat(self, wav_dir: PosixPath) -> Tuple[PosixPath, PosixPath]:
-        wav_name = wav_dir.stem
-        wav_parent_dir = wav_dir.parent
-        dat_dir = wav_parent_dir.joinpath(f"{wav_name}.dat")
-        return wav_dir, dat_dir
+    def pair_dat_wav(self, dat_dir: PosixPath) -> Tuple[PosixPath, PosixPath]:
+        dat_name = dat_dir.stem
+        dat_parent_dir = dat_dir.parent
+        wav_dir = dat_parent_dir.joinpath(f"{dat_name}.wav")
+        return dat_dir, wav_dir
